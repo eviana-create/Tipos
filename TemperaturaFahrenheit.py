61c0fb145ebab6991ffdfade1a338d119d429719
Emerson Viana <missonzito@outlook.com>
2025-03-25 22:02:34 -0300

    Calculo de conversão de Temperatura em fahreinhet para Celsius

---
+++ TemperaturaFahrenheit.py
@@ -0,0 +1,4 @@
+temperaturaFahrenheit = int(input("Digite uma tempreratura: "))
+temp = float(temperaturaFahrenheit)
+temperaturaCelsius = (temperaturaFahrenheit - 32) * 5 / 9
+print("A Temperatura em Celsius é: ", temperaturaCelsius)
