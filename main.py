2119c7bb1d6571e2c3e483423572b695a7c83782
Emerson Viana <missonzito@outlook.com>
2025-03-25 21:49:22 -0300

    Propriedade len

---
+++ main.py
@@ -0,0 +1,15 @@
+cumprimento = "Olá"
+nome = "Ana"
+turno = "Bom Dia"
+
+
+
+tamanho = len(cumprimento) + len(nome) + len(turno)
+print(tamanho)
+
+m = 22 % 3
+print(m)
+
+y = 2 * 1 ** 2
+print(y)
+
