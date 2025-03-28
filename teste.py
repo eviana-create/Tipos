c8b1dbd92e1e01b2cc24bf3ddc77dcf839fd169d
Emerson Viana <missonzito@outlook.com>
2025-03-27 21:09:27 -0300

    testando Variaveis para praticar

---
+++ teste.py
@@ -0,0 +1,39 @@
+print("Ola")
+print()
+print("Bom Dia")
+
+a = 10
+b = 20
+c = a
+b = c
+a = b
+print(a, b, c)
+
+a = 10
+b = 5
+c = a + b
+b = 20
+print(a, b, c)
+
+A = 10
+a = 20
+a = 2 * a
+A = a + A
+print(a)
+
+print("Ola"  'Mundo')
+
+a = int(input("Qual o valor de a ? "))
+b = int(input("Qual o valor de b ? "))
+a = b
+b = a
+print(a)
+print(b)
+
+a = int(input("Qual o valor de a ? "))
+b = int(input("Qual o valor de b ? "))
+aux = a
+a = b
+b = aux
+print("a variavel a agora é: ", a)
+print("a variavel b agora é: ", b)
